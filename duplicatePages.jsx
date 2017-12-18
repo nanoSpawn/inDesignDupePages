767334af4ca0f217b21eb28f01e8da8821e4c297
jose <nanospawn@gmail.com>
2017-12-18 10:59:16 +0100

    First commit

---
+++ duplicatePages.jsx
@@ -0,0 +1,48 @@
+if (app.documents.length == 0){
+    alert ("Abrid un documento, seleccionad un objeto y volved a probar.");
+    exit();
+}
+
+var myDoc = app.activeDocument;
+var myPage = myDoc.layoutWindows[0].activePage;
+var myPageId = myPage.id;
+var myDialog = app.dialogs.add({name:"Duplicar páginas"});
+
+
+with(myDialog.dialogColumns.add()){
+    staticTexts.add({staticLabel:"Hay que pensar que la primera página ha de recolocarse a mano."});
+        with(dialogRows.add()) {
+            staticTexts.add({staticLabel:"Duplicar página seleccionada # " + myPage.name});
+            var myIntBox = integerEditboxes.add({editValue:1, smallNudge:1, largeNudge:10});
+            staticTexts.add({staticLabel:" veces\n"});
+        }
+        
+        with(dialogRows.add()){
+            var inverted = checkboxControls.add({staticLabel:'Páginas invertidas'});
+        }
+}
+
+var myResult = myDialog.show();
+
+if(myResult == true){
+    var myNumber = myIntBox.editValue;
+    if (myNumber < 1) {
+        alert(myNumber + " es un número inválido.");
+        exit();
+    }
+    
+    var inv = inverted.checkedState; // Aquí hay que leer el checkbox
+    //alert(inverted.checkedState);
+    
+    if (inv == true) {
+         for (i = 0; i < myNumber; i++) {
+         myPage.duplicate(LocationOptions.AFTER, myDoc.pages.itemByID(myPageId));
+        }
+    } else  {
+        for (i = 0; i < myNumber; i++) {
+        myPage.duplicate(LocationOptions.BEFORE, myDoc.pages.itemByID(myPageId));
+        }  
+    }
+}
+
+myDialog.destroy();
